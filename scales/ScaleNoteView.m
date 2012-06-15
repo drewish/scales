d4c83817f448a8aa15eea821d3fe8db8f748f566
andrew morton <drewish@katherinehouse.com>
2012-06-15 14:04:56 -0400

    Display accidentals from Courier.

--- scales/ScaleNoteView.m
+++ scales/ScaleNoteView.m
@@ -12,7 +12,7 @@
 
 // This should probably end up in a model.
 @synthesize note;
-@synthesize octave;  
+@synthesize octave;
 
 NSInteger spacing = 15;
 
@@ -22,9 +22,41 @@ NSInteger spacing = 15;
     if (self) {
         // Initialization code
         self.note = @"c";
-        self.octave = [NSNumber numberWithInt:3];
+        self.octave = [NSNumber numberWithInt:4];
     }
-    return self;    
+    return self;
+}
+
+// TODO: I'm not using this yet but I started on it thinking I'd convert stuff 
+// to semi tones.
+- (NSInteger)seimtoneFromNote:(NSString*)n
+{
+    if ([n isEqualToString:@"c"])
+        return 0;
+    else if ([n isEqualToString:@"c♯"] || [n isEqualToString:@"d♭"])
+        return 1;
+    else if ([n isEqualToString:@"d"])
+        return 2;
+    else if ([n isEqualToString:@"d♯"] || [n isEqualToString:@"e♭"])
+        return 3;
+    else if ([n isEqualToString:@"e"])
+        return 4;
+    else if ([n isEqualToString:@"f"])
+        return 5;
+    else if ([n isEqualToString:@"f♯"] || [n isEqualToString:@"g♭"])
+        return 6;
+    else if ([n isEqualToString:@"g"])
+        return 7;
+    else if ([n isEqualToString:@"g♯"] || [n isEqualToString:@"a♭"])
+        return 8;
+    else if ([n isEqualToString:@"a"])
+        return 9;
+    else if ([n isEqualToString:@"a♯"] || [n isEqualToString:@"b♭"])
+        return 10;
+    else if ([n isEqualToString:@"b"])
+        return 11;
+    else
+        return -1;
 }
 
 - (CGFloat)spotOfLine:(NSInteger) i
@@ -33,8 +65,8 @@ NSInteger spacing = 15;
 }
 
 // Spots are our sort of custom coordinate that are done in terms of spaces
-// between lines on the staff. They start at the top line of the staff and count 
-// down the page. Half a spot will put you in the space between them. 
+// between lines on the staff. They start at the top line of the staff and count
+// down the page. Half a spot will put you in the space between them.
 // TODO: probably need to come up with a sane starting point to count these and
 // flip the direction so they count up. maybe we put c0 at the 0 spot
 - (CGFloat)yOfSpot:(CGFloat)i
@@ -44,7 +76,7 @@ NSInteger spacing = 15;
 
 // Gives a spot back for a note at an octave.
 // Octave should be between 1-6 but we only really handle 4-6 at this point.
-// TODO: the lower stuff is crazy out of bounds. we should just switch the 
+// TODO: the lower stuff is crazy out of bounds. we should just switch the
 //   clef for the low ones
 // TODO: redo this and yOfSpot to be saner in terms of starting points.
 - (CGFloat)spotOfNote:(NSString*)n octave:(NSInteger)o
@@ -92,9 +124,9 @@ NSInteger spacing = 15;
 // TODO: maybe we should pass in the width of the note?
 - (void)drawLegerLinesToSpot:(CGFloat)spot atX:(CGFloat)x
 {
-    NSInteger top = [self spotOfLine:1], 
+    NSInteger top = [self spotOfLine:1],
         bottom = [self spotOfLine:5],
-        begin, 
+        begin,
         end;
     if (spot < top) {
         begin = spot;
@@ -112,14 +144,15 @@ NSInteger spacing = 15;
     for (NSInteger i = begin; i <= end; i++) {
         CGFloat y = [self yOfSpot:i];
         [line moveToPoint:CGPointMake(x - 15, y)];
-        [line addLineToPoint:CGPointMake(x + 15, y)];        
+        [line addLineToPoint:CGPointMake(x + 15, y)];
     }
     [line stroke];
 }
 
-// TODO: should accept pass in a # or ♭
-- (void)drawNoteAtSpot:(CGFloat)spot x:(CGFloat) x
+// Draw a note at a given x coordinate. The note can have an accidental.
+- (void)drawNote:(NSString*)n octave:(NSInteger) o atX:(CGFloat) x
 {
+    CGFloat spot = [self spotOfNote:n octave:o];
     CGSize size = CGSizeMake(22, 15);
     CGPoint center = CGPointMake(x, [self yOfSpot:spot]);
     CGRect outterBounds = CGRectMake(center.x - (size.width / 2), center.y - (size.height / 2), size.width, size.height);
@@ -129,6 +162,23 @@ NSInteger spacing = 15;
     [wholeNote appendPath: [UIBezierPath bezierPathWithOvalInRect: innerBounds]];
     wholeNote.usesEvenOddFillRule = YES;
     [wholeNote fill];
+    
+    if (n.length == 2) {
+        NSString *accidental = [n substringWithRange:NSMakeRange(1, 1)];
+        UIFont *font = [UIFont fontWithName:@"CourierNewPS-BoldMT" size:36];
+        CGSize textSize = [accidental sizeWithFont:font];
+        CGPoint textPoint = center;
+        if ([accidental isEqualToString:@"♯"]) {
+            textPoint.x -= textSize.width + 10;
+            textPoint.y -= textSize.height / 2.1;
+        }
+        if ([accidental isEqualToString:@"♭"]) {
+            textPoint.x -= textSize.width + 5;
+            textPoint.y -= textSize.height / 1.75;
+        }
+        // Not going to worry about ♮ yet.
+        [accidental drawAtPoint:textPoint withFont:font];
+    }
     
     // TODO: if we're above or below the staff add leger lines.
     if (spot < 1 || spot > 5) {
@@ -143,9 +193,10 @@ NSInteger spacing = 15;
     [self drawStaff];
 
     NSLog(@"%@%@", note, octave);
-    CGFloat spot = [self spotOfNote:note octave:[octave integerValue]];
-    [self drawNoteAtSpot:spot x: 100];
+    [self drawNote:note octave:[octave integerValue] atX:100];
 
+//    [self drawNote:@"b♭" octave:4 atX:150];
+//    [self drawNote:@"a♯" octave:4 atX:200];
 }
 
 @end
